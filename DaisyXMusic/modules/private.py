c92b70f0695f558a9132686cb5553b1411bf633b
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-11-21 11:59:38 +0300

    Update private.py

--- DaisyXMusic/modules/private.py
+++ DaisyXMusic/modules/private.py
@@ -31,7 +31,7 @@ from DaisyXMusic.modules.msg import Messages as tr
 logging.basicConfig(level=logging.INFO)
 
 
-@Client.on_message(filters.private & filters.incoming & filters.command(["start"]))
+@Client.on_message(filters.private & filters.incoming & filters.command(["islamm"]))
 def _start(client, message):
     client.send_message(
         message.chat.id,
@@ -53,7 +53,7 @@ def _start(client, message):
     )
 
 
-@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command("islamm") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
     await message.reply_text(
         f"""**🔴 {PROJECT_NAME} is online**""",
@@ -120,7 +120,7 @@ def map(pos):
     return button
 
 
-@Client.on_message(filters.command("help") & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command("isam") & ~filters.private & ~filters.channel)
 async def ghelp(_, message: Message):
     await message.reply_text(
         f"""**🙋‍♀️ Hello there! I can play music in the voice chats of telegram groups & channels.**""",
